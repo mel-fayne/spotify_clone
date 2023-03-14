cfe85ebc0f704b2773e0c57a9320de6760dd0ba9
mel-fayne <melaniefayne33@gmail.com>
2023-03-14 22:47:57 +0300

    implement song details components

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -41,83 +41,83 @@ const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handle
 );
 
 const TopPlay = () => {
-  const dispatch = useDispatch();
-  const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data } = useGetTopChartsQuery();
-  const divRef = useRef(null);
+  // const dispatch = useDispatch();
+  // const { activeSong, isPlaying } = useSelector((state) => state.player);
+  // const { data } = useGetTopChartsQuery();
+  // const divRef = useRef(null);
 
-  useEffect(() => {
-    divRef.current.scrollIntoView({ behavior: 'smooth' });
-  });
+  // useEffect(() => {
+  //   divRef.current.scrollIntoView({ behavior: 'smooth' });
+  // });
 
-  const topPlays = data.tracks?.slice(0, 5);
+  // const topPlays = data.tracks?.slice(0, 5);
 
-  const handlePauseClick = () => {
-    dispatch(playPause(false));
-  };
+  // const handlePauseClick = () => {
+  //   dispatch(playPause(false));
+  // };
 
-  const handlePlayClick = (song, i) => {
-    dispatch(setActiveSong({ song, data, i }));
-    dispatch(playPause(true));
-  };
+  // const handlePlayClick = (song, i) => {
+  //   dispatch(setActiveSong({ song, data, i }));
+  //   dispatch(playPause(true));
+  // };
 
-  return (
-    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
-      <div className="w-full flex flex-col">
-        <div className="flex flex-row justify-between items-center">
-          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
-          <Link to="/top-charts">
-            <p className="text-gray-300 text-base cursor-pointer">See more</p>
-          </Link>
-        </div>
+  // return (
+  //   <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+  //     <div className="w-full flex flex-col">
+  //       <div className="flex flex-row justify-between items-center">
+  //         <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+  //         <Link to="/top-charts">
+  //           <p className="text-gray-300 text-base cursor-pointer">See more</p>
+  //         </Link>
+  //       </div>
 
-        <div className="mt-4 flex flex-col gap-1">
-          {topPlays?.map((song, i) => (
-            <TopChartCard
-              key={song.key}
-              song={song}
-              i={i}
-              isPlaying={isPlaying}
-              activeSong={activeSong}
-              handlePauseClick={handlePauseClick}
-              handlePlayClick={() => handlePlayClick(song, i)}
-            />
-          ))}
-        </div>
-      </div>
+  //       <div className="mt-4 flex flex-col gap-1">
+  //         {topPlays?.map((song, i) => (
+  //           <TopChartCard
+  //             key={song.key}
+  //             song={song}
+  //             i={i}
+  //             isPlaying={isPlaying}
+  //             activeSong={activeSong}
+  //             handlePauseClick={handlePauseClick}
+  //             handlePlayClick={() => handlePlayClick(song, i)}
+  //           />
+  //         ))}
+  //       </div>
+  //     </div>
 
-      <div className="w-full flex flex-col mt-8">
-        <div className="flex flex-row justify-between items-center">
-          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
-          <Link to="/top-artists">
-            <p className="text-gray-300 text-base cursor-pointer">See more</p>
-          </Link>
-        </div>
+  //     <div className="w-full flex flex-col mt-8">
+  //       <div className="flex flex-row justify-between items-center">
+  //         <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+  //         <Link to="/top-artists">
+  //           <p className="text-gray-300 text-base cursor-pointer">See more</p>
+  //         </Link>
+  //       </div>
 
-        <Swiper
-          slidesPerView="auto"
-          spaceBetween={15}
-          freeMode
-          centeredSlides
-          centeredSlidesBounds
-          modules={[FreeMode]}
-          className="mt-4"
-        >
-          {topPlays?.slice(0, 5).map((artist) => (
-            <SwiperSlide
-              key={artist?.key}
-              style={{ width: '25%', height: 'auto' }}
-              className="shadow-lg rounded-full animate-slideright"
-            >
-              <Link to={`/artists/${artist?.artists[0].adamid}`}>
-                <img src={artist?.images?.background} alt="Name" className="rounded-full w-full object-cover" />
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </div>
-  );
+  //       <Swiper
+  //         slidesPerView="auto"
+  //         spaceBetween={15}
+  //         freeMode
+  //         centeredSlides
+  //         centeredSlidesBounds
+  //         modules={[FreeMode]}
+  //         className="mt-4"
+  //       >
+  //         {topPlays?.slice(0, 5).map((artist) => (
+  //           <SwiperSlide
+  //             key={artist?.key}
+  //             style={{ width: '25%', height: 'auto' }}
+  //             className="shadow-lg rounded-full animate-slideright"
+  //           >
+  //             <Link to={`/artists/${artist?.artists[0].adamid}`}>
+  //               <img src={artist?.images?.background} alt="Name" className="rounded-full w-full object-cover" />
+  //             </Link>
+  //           </SwiperSlide>
+  //         ))}
+  //       </Swiper>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default TopPlay;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -11,9 +11,13 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => 'charts/track' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `songs/get-details?key=${songid}&locale=en-US` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `songs/get-related-artist?id=${songid}&l=en-US` }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
 } = shazamCoreApi;
